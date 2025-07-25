3aa904a1aaf19b4e11175ea19aea1591cf5b90b7
Yumeth <yumethsumathipala@gmail.com>
2025-07-26 01:06:49 +0530

    Update API base URL for backend connection
    Change the base URL in the axios instance to point to the new
    backend server while commenting out the previous URL for
    reference. This ensures the application connects to the correct
    API endpoint.

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const api = axios.create({
   //  baseURL: API_BASE_URL,
   
-  baseURL:"https://backend-v2-f059.onrender.com"
+  // baseURL:"https://backend-v2-f059.onrender.com"
+  baseURL:"http://ec2-13-212-91-167.ap-southeast-1.compute.amazonaws.com:5000"
 });
 
 export default api;
