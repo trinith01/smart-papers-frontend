892f2e3e61d62447517bcc9b26595620f14e9fba
Yumeth <yumethsumathipala@gmail.com>
2025-08-12 19:51:58 +0530

    Update API base URL to use the production endpoint
    Change the base URL in the axios instance to point to the live
    API endpoint for better accessibility and performance.

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   //  baseURL: API_BASE_URL,
   
   // baseURL:"https://backend-v2-f059.onrender.com"
-  baseURL:"http://ec2-13-212-91-167.ap-southeast-1.compute.amazonaws.com:5000"
+  baseURL:"https://api.qgen.edu.lk",
 });
 
 export default api;
