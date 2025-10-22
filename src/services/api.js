b8f032b05b2afa1752f718a39a704b1436877840
trinith01 <gt202054194@gmail.com>
2025-10-22 10:05:08 +0530

    fix: Update API base URL to use the production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 const api = axios.create({
   //  baseURL: API_BASE_URL,
   
-  baseURL:"http://localhost:5000"
-  //baseURL:"https://api.qgen.edu.lk",
+  //baseURL:"http://localhost:5000"
+  baseURL:"https://api.qgen.edu.lk",
 });
 
 export default api;
